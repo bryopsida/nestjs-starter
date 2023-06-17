e7d37c60170467c77db8d6a9bd3e44953d4043e2
bryopsida <8363252+bryopsida@users.noreply.github.com>
2023-06-17 16:29:51 -0500

    add pino logging

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { ConfigModule } from '@nestjs/config'
+import { HealthModule } from './health/health.module'
+import { LoggerModule } from 'nestjs-pino'
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), LoggerModule.forRoot(), HealthModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
-
+import { Logger } from 'nestjs-pino'
 import helmet from 'helmet'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule, { bufferLogs: true })
+  app.useLogger(app.get(Logger))
+
   const config = new DocumentBuilder()
     .setTitle('Nest.JS Starter')
     .setDescription('A Nest.JS Starter Project')
