b8f699d917a293d1899a5c19153d4c9473b13e6e
bryopsida <8363252+bryopsida@users.noreply.github.com>
2023-06-17 16:21:34 -0500

    add compodoc, openapi, helmet

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+import helmet from 'helmet'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  const config = new DocumentBuilder()
+    .setTitle('Nest.JS Starter')
+    .setDescription('A Nest.JS Starter Project')
+    .setVersion('1.0')
+    .addTag('starter')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.use(helmet());
   await app.listen(3000)
 }
 bootstrap()
