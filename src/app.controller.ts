5e23eac391f806fe872ef23acf37deac4701973a
bryopsida <8363252+bryopsida@users.noreply.github.com>
2023-06-17 15:30:31 -0500

    standardjs style

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get } from '@nestjs/common'
+import { AppService } from './app.service'
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  private readonly appService: AppService
+  constructor(appService: AppService) {
+    this.appService = appService
+  }
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return this.appService.getHello()
   }
 }
